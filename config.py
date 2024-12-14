6ae9d585dc08b4f697aa1be1f9ddfc12d9e3e57c
Michael Ishak <mishak19193@gmail.com>
2024-12-14 15:48:19 -0500

    uploaded sample config.py so that the driver doesnt force you to log in with a QR code every time.

---
+++ config.py
@@ -0,0 +1,3 @@
+# This is here so you don't need to log in with QR code every time you run the script.
+WEBDRIVER_PATH = "C:\\WebDrivers\\msedgedriver.exe"
+USER_DATA_DIR = "C:\\Users\\YourUsername\\AppData\\Local\\Microsoft\\Edge\\User Data"
