777b1605885ca0302987a4b1ba092b7ce29699ea
Michael Ishak <mishak19193@gmail.com>
2024-12-14 22:26:05 -0500

    Remove config.py from tracking and keep only config_sample.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# This is here so you don't need to log in with QR code every time you run the script.
-WEBDRIVER_PATH = "C:\\WebDrivers\\msedgedriver.exe"
-USER_DATA_DIR = "C:\\Users\\misha\\AppData\\Local\\Microsoft\\Edge\\User Data"
-PROFILE = "Default"
