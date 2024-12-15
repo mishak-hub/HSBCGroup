2b1479c37e3195f62345e25de699b98ecab7d5bd
Michael Ishak <mishak19193@gmail.com>
2024-12-14 22:22:25 -0500

    changes made to config

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # This is here so you don't need to log in with QR code every time you run the script.
 WEBDRIVER_PATH = "C:\\WebDrivers\\msedgedriver.exe"
-USER_DATA_DIR = "C:\\Users\\YourUsername\\AppData\\Local\\Microsoft\\Edge\\User Data"
+USER_DATA_DIR = "C:\\Users\\misha\\AppData\\Local\\Microsoft\\Edge\\User Data"
+PROFILE = "Default"
