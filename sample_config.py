f699658363926fc9e84b81e08cb29d762cbfc129
mishak-hub <55812903+mishak-hub@users.noreply.github.com>
2024-12-14 22:28:58 -0500

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,4 @@
+# This is here so you don't need to log in with QR code every time you run the script.
+WEBDRIVER_PATH = "C:\\WebDrivers\\msedgedriver.exe" #Enter your own path to your webdriver
+USER_DATA_DIR = "C:\\Users\\<YOUR_USER_HERE>\\AppData\\Local\\Microsoft\\Edge\\User Data" #Change username
+PROFILE = "Default" # likely your profile name unless you changed it
